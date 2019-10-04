70f69f02b033e9764fde64d3a9fab086ed0b3e6e
Titus Buchanan Jr <titusbuchananjr@gmail.com>
2019-10-03 22:51:16 -0400

    new program that goes through multiple text programs

--- organize.js
+++ organize.js
@@ -1,60 +1,50 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Teezus News</title>
-</head>
-<body>
-    <form>
-        News: <input id ="newstext" name = "city">
-        <input id = "news" type = "button" value = "click for Teezy News" onclick ="loadData()">
-    </form>
+var fileFoo = require('fs'); // 'fs' = file system the require calls the file system
+
+//1.load_dept_names.txt
+fileFoo.readFile('load_dept_names.txt', 'utf8', function (error, fileData) {
+    if (error) throw (error);
+
+    var newFileData = fileData.replace(/\(|\'|\),|\);/g, "");
+
+    var newArrayFileData = newFileData.split("\n");
+    newArrayFileData.shift();
 
 
-    <div id ="text"></div>
-    <div id ="information"></div>
-    <div id ="paper"></div>
 
-    <script>
-    document.getElementById("newstext").addEventListener('click',loadData);
-
-
-    function loadData(){
-        
-        
-        var newsElement = document.getElementById("newstext");
-        var newsName = newsElement.value;
-        
-        //step 1
-        var xhr = new XMLHttpRequest();
-        console.log(xhr);
-        //step 2
-        xhr.open('GET','https://newsapi.org/v2/top-headlines?country=us'+ newsName +'&apiKey=0839cc440e15485f8bfc20033f9d0420',true);
-
-        //step 3
-        xhr.onload = function(){
-            if(xhr.status == 200){
-                console.log(xhr.responseText);
-                
-                //step 5
-                var parsed =JSON.parse(xhr.responseText)
-                console.log(parsed);
-
-                document.getElementById("text").innerText = "Author: "+ parsed.articles[0].author;
-                document.getElementById("information").innerText = "Headline: "+ parsed.articles[0].content;
-                document.getElementById("paper").innerText = "Story: "+ parsed.articles[0].description;
-
-            }
-        }
-
-        //step 4
-        xhr.send();
+    var departmentNames = [];
+    for (var i = 0; i < newArrayFileData.length; i++) {
+        departmentNames.push(newArrayFileData[i].split(","));
     }
+    console.log(departmentNames);
 
 
-    </script>
+});
+
+//2.
+
+
+
+fileFoo.readFile('load_dept_emp.txt', 'utf8', function (errorTwo, fileDataTwo) {
+    if (errorTwo) throw (errorTwo);
+
+    console.log(fileDataTwo);
+
+    var newFileDataTwo = fileDataTwo.replace(/\(|\'|\),/g, "")
+
     
-</body>
-</html>
+
+
+    var newArrayFileDataTwo = newFileDataTwo.split("\n")
+    newArrayFileDataTwo.shift();
+
+    console.log(newArrayFileDataTwo);
+
+    var departmentNames = [];
+    for(var i = 0; i < newArrayFileDataTwo.length; i++){
+        if(newArrayFileDataTwo[i].length > 0){
+           departmentNames.push(newArrayFileDataTwo[i].split(","))
+        }
+    }
+console.log(departmentNames);
+})
+
